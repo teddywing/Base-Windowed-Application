3d8cf2d206f62bf16bb5be222ebab4100fa380f6
Teddy Wing <git@teddywing.com>
2023-09-29 22:28:49 +0200

    AppDelegate: Use consistent brace format
    Match the brace format from "Document.h".

--- src/AppDelegate.h
+++ src/AppDelegate.h
@@ -1,6 +1,7 @@
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate> {
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
 	NSWindow *_window;
 	NSScrollView *_scroll_view;
 	NSTextView *_text_view;
